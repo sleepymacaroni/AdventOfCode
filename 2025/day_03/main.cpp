94956db4ea0378019282af9df8deb0d7f892dbcb
sashapancakes <sweingarten0718@sdsu.edu>
2025-12-05 00:45:06 -0800

    day 3, part 2

--- 2025/day_03/main.cpp
+++ 2025/day_03/main.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+long long maxJoltage(const string& bank) {
+    vector<char> digits;
+    digits.push_back(-1);
+    for (int i = 0; i < bank.size(); i++) {
+        while (!digits.empty() && (digits.size() + bank.size() - i - 1 >= 12) && digits.back() < bank[i]) {
+            digits.pop_back();
+        }
+        if (digits.size() < 12) {
+            digits.push_back(bank[i]);
+        }
+    }
+    string cat = string(digits.begin(), digits.end());
+    return stoll(cat);
+}
 
 int main() {
     ifstream inputStream("input.txt");
     string bank;
-    int total = 0;
+    long long total = 0;
     while (getline(inputStream, bank)) {
-        int digit1 = -1;
-        int max = -1;
-        for (int i = 0; i < bank.length(); i++) {
-            for (int j = i+1; j < bank.length(); j++) {
-                if ((bank[i]-'0') * 10 + (bank[j]-'0') > max) {
-                    max = (bank[i]-'0') * 10 + (bank[j]-'0');
-                }
-            }
-        }
-        total += max;
+        total += maxJoltage(bank);
     }
     cout << total << endl;
 }
